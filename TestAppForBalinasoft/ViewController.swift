a53e8f4d20b403f4dcb280aa0240067e8a31a5b9
SDubko <sdubko@ibagroup.eu>
2023-09-24 15:34:48 +0300

    Initial Commit

---
+++ TestAppForBalinasoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppForBalinasoft
+//
+//  Created by Dubko Siarhei on 24.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
